b60dae6ad172ff643a389ee193a5f0fdd74b8631
Alejandro314 <alejandro_roman_caballero@outlook.es>
2019-03-04 16:19:21 +0100

    Cambiando una cosa de carga.sql y inicioservicio.sh

--- inicioservicio.sh
+++ inicioservicio.sh
@@ -5,7 +5,7 @@
 
 sed -i 's/$db_user="condoriano"/$db_user="'"$user_2"'"/g' /var/www/html/PROYECTO_IAW_ROMAN_CABALLERO/PAGINA_WEB/var.php
 sed -i 's/$db_host="localhost"/$db_host="'"$host"'"/g' /var/www/html/PROYECTO_IAW_ROMAN_CABALLERO/PAGINA_WEB/var.php
-sed -i 's/$db_password="890iopklñ"/$db_password="'"$contra_2"'"/g' /var/www/html/PROYECTO_IAW_ROMAN_CABALLERO/PAGINA_WEB/var.php
+sed -i 's/$db_password="890iopkln"/$db_password="'"$contra_2"'"/g' /var/www/html/PROYECTO_IAW_ROMAN_CABALLERO/PAGINA_WEB/var.php
 sed -i 's/$db_name="alebuntu"/$db_name="'"$gbd"'"/g' /var/www/html/PROYECTO_IAW_ROMAN_CABALLERO/PAGINA_WEB/var.php
 
 
@@ -13,7 +13,7 @@ sed -i 's/root/'"$user_1"'/g' /usr/bin/carga.sql
 sed -i 's/condoriano/'"$user_2"'/g' /usr/bin/carga.sql
 sed -i 's/localhost/'"$host"'/g' /usr/bin/carga.sql
 sed -i 's/asdqwe123/'"$contra_1"'/g' /usr/bin/carga.sql
-sed -i 's/890iopklñ/'"$contra_2"'/g' /usr/bin/carga.sql
+sed -i 's/890iopkln/'"$contra_2"'/g' /usr/bin/carga.sql
 sed -i 's/alebuntu/'"$gbd"'/g' /usr/bin/carga.sql
 
 
